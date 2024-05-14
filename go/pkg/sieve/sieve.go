3634e0734a6367ff6969b1e6b13e286097374812
Elijah Brown <elijah@ad7zj.net>
2024-05-14 01:05:40 -0700

    Basic functionality is working now, using a segmented sieve to limit memory usage

--- go/pkg/sieve/sieve.go
+++ go/pkg/sieve/sieve.go
@@ -1,11 +1,13 @@
 package sieve
 
 import (
-	//"fmt"
+	"fmt"
+	"math"
 )
 
 type sieveImpl struct{
-	sieve []bool 
+	primes []int64
+	segmentedPrimes []int64
 }
 
 type Sieve interface {
@@ -14,37 +16,85 @@ type Sieve interface {
 
 func NewSieve() Sieve {
     return &sieveImpl{
-        sieve: make([]bool, 200),
     }
 }
+
 
 func (s *sieveImpl) NthPrime(n int64) int64 {
-	// create an array to hold the necessary number of primes
-	//s.sieve = make([]bool, n+1)
-	var primes []int64        // List to store prime numbers
-    var num int64 = 2         // Start with the first prime number
-
-    // Loop until we find the n'th prime number
-    for int64(len(primes)) < (n + 1) {
-        // If num is marked as prime, add it to the list of primes
-        if !s.sieve[num] {
-            primes = append(primes, num)
-
-            // Mark multiples of num as non-prime
-            for i := num * num; i < int64(len(s.sieve)); i += num {
-                s.sieve[i] = true
-            }
-        }
-
-        // Move to the next number
-        num++
-    }
+	s.SegmentedSieve(n*n)
 
     // Return the n'th prime number
-    return primes[n]
-
-
-
-
+    return append(s.primes, s.segmentedPrimes...)[n]
 }
 
+
+
+func (s *sieveImpl) RegularSieve(limit int64) {
+    sieve := make([]bool, limit)
+    // loop through numbers up to the square root of the limit
+    for i := int64(2); i*i <= limit; i++ {
+        if !sieve[i] {
+            // mark multiples as non-prime
+            for j := i * i; j < limit; j += i {
+                sieve[j] = true // true indicates this index is not prime
+            }
+        }
+    }
+
+    for i := 2; i < len(sieve); i++ {
+    	if !sieve[i] {
+        	s.primes = append(s.primes, int64(i))
+        	fmt.Printf("r%d ", i)
+        }
+    }
+}
+
+
+func (s *sieveImpl) SegmentedSieve(limit int64) {
+	// divide the range into segments roughly sqrt(limit)
+	segment := int64(int(math.Floor(math.Sqrt(float64(limit))) + 1))
+
+	// calculate primes for the base range using the regular sieve
+	s.RegularSieve(segment)
+
+	// calculate upper and lower bounds for this segment
+	segLo := segment
+	segHi := segment * 2
+
+	// Loop over each segment
+	for segLo < limit {
+		if segHi >= limit {
+			segHi = limit
+		}
+
+		// allocate space for our sieve (this also clears marks from the last time)
+		sieve := make([]bool, segment+1)
+
+		// loop through all the primes found by RegularSieve() to find primes in current range
+		for _, p := range s.primes {
+			// Find the minimum number in this range [segLo...segHi] that is divisible (not necessarily evenly) by this prime.
+			// As an example, if segLo is 11 and current prime is 3, we start with 12.
+			loLim := int64(math.Floor(float64(segLo) / float64(p)) * float64(p))
+			if loLim < segLo {
+				loLim += p
+			}
+
+			// Mark multiples of this prime within this range as non-prime
+			for j := loLim; j < segHi; j += p {
+				sieve[j-segLo] = true // true indicates this index is not prime
+			}
+		}
+
+		// Now collect our primes
+		for i := segLo; i < segHi; i++ {
+			if !sieve[i-segLo] {
+				s.segmentedPrimes = append(s.segmentedPrimes, int64(i))
+				fmt.Printf("%d ", i)
+			}
+		}
+
+		// Update segLo and segHi for next segment
+		segLo += segment
+		segHi += segment
+	}
+}
