f40712875701c065f2ba6ee3d41063e77c3b2391
Elijah Brown <elijah@ad7zj.net>
2024-05-15 01:08:01 -0700

    Add support for command line arguments in test cmd file

--- go/pkg/sieve/cmd/sieve_main.go
+++ go/pkg/sieve/cmd/sieve_main.go
@@ -2,12 +2,26 @@ package main
 
 import (
     "fmt"
+    "os"
+    "strconv"
     "ssse-exercise-sieve/pkg/sieve"
 )
 
 func main() {
     sieve := sieve.NewSieve()
-    n := 19
+
+    // Check if the correct number of arguments are provided
+    if len(os.Args) != 2 {
+        fmt.Println("Usage: go run sieve_main.go <n'th prime to return>")
+        return
+    }
+
+    // Parse the argument as an integer
+    n, err := strconv.Atoi(os.Args[1])
+    if err != nil {
+        fmt.Println("Invalid number:", os.Args[1])
+        return
+    }
 
     fmt.Printf("%dth prime number is %d\n", n, sieve.NthPrime(int64(n)))
 }
